fddc08fdd003b82e36808b057f161304c473dc9e
Svitlana Yudina <osbka31@gmail.com>
2023-03-21 12:19:17 +0200

    get user by ID was implemented

--- src/app.js
+++ src/app.js
@@ -20,8 +20,25 @@ server.get('/users', async(req, res) => {
   try {
     const { role } = req.query;
 
-    const usersToSend = role
-      ? await client.query(`
+    if (role === undefined) {
+      const allUsers = await client.query(`
+        SELECT
+          users.username,
+          profiles.first_name,
+          profiles.last_name,
+          users.email,
+          users.role,
+          profiles.state
+        FROM users JOIN profiles
+        ON users.profile_id = profiles.id
+      `);
+
+      res.send(allUsers.rows);
+      return;
+    }
+
+    if (role === 'user' || role === 'admin') {
+      const usersToSend = await client.query(`
         SELECT
           users.username,
           profiles.first_name,
@@ -32,24 +49,46 @@ server.get('/users', async(req, res) => {
         FROM users JOIN profiles
         ON users.profile_id = profiles.id
         WHERE users.role = $1
-        `, [role])
-      : await client.query(`
-        SELECT
-          users.username,
-          profiles.first_name,
-          profiles.last_name,
-          users.email,
-          users.role,
-          profiles.state
-        FROM users JOIN profiles
-        ON users.profile_id = profiles.id
-        `);
+      `, [role]);
 
-    res.send(usersToSend.rows);
+      res.send(usersToSend.rows);
+      return;
+    }
+
+    res.statusCode = 422;
+    res.send(`The role must be only 'user' or 'admin'. Try another request.`);
   } catch (err) {
     res.send('Something went wrong:( We will fix this problem soon');
   }
 });
+
+server.get('/users/:userId', async(req, res) => {
+  try {
+    const { userId } = req.params;
+    const foundedUser = await client.query(`
+      SELECT
+        users.username,
+        profiles.first_name,
+        profiles.last_name,
+        users.email,
+        users.role,
+        profiles.state
+      FROM users JOIN profiles
+      ON users.profile_id = profiles.id
+      WHERE users.id = $1
+    `, [Number(userId)]);
+    
+    if (foundedUser.rows.length === 0) {
+      res.statusCode = 404;
+      res.send('UserId not found');
+      return;
+    }
+
+    res.send(foundedUser.rows);
+  } catch (err) {
+    res.send('Something went wrong:( We will fix this problem soon');
+  }
+})
 
 server.listen(port, () => {
   console.log('server start on port', port);
