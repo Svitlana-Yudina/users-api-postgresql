b94d186ff198d783d7b15c1e6cef73e2dd739d23
Svitlana Yudina <osbka31@gmail.com>
2023-03-22 14:53:36 +0200

    controllers were created

--- src/app.js
+++ src/app.js
@@ -4,188 +4,32 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import * as userServices from './services/users.js';
+import * as userControllers from './controllers/users.js';
 
 const server = express();
 const port = process.env.PORT || 5000;
 
 server.use(cors());
 server.use(express.static(path.resolve('public')));
-server.use(express.json());
 
 server.get('/users', async(req, res) => {  
-  try {
-    const { role } = req.query;
+  const { role } = req.query;
 
-    if (role === undefined) {
-      const allUsers = await userServices.getAll();
-      
-      res.send(allUsers);
-      return;
-    }
-
-    if (role === 'user' || role === 'admin') {
-      const usersToSend = await userServices.getByRole(role);
-      
-      res.send(usersToSend);
-      return;
-    }
-
-    res.statusCode = 422;
-    res.send(`The role must be only 'user' or 'admin'. Try another request.`);
-  } catch (err) {
-    res.send('Something went wrong:( We will fix this problem soon');
+  if (role === undefined) {
+    await userControllers.getAll(req, res);
+    return;
   }
+
+  await userControllers.getByRole(req, res);
 });
 
-server.get('/users/:userId', async(req, res) => {
-  try {
-    const { userId } = req.params;
+server.get('/users/:userId', userControllers.getOne);
 
-    if (!Number(userId)) {
-      res.statusCode = 422;
-      res.send('UserId must be a number bigger than "0"');
-      return;
-    }
+server.post('/users', express.json(), userControllers.create);
 
-    const foundedUser = await userServices.getById(userId);
-    
-    if (!foundedUser) {
-      res.statusCode = 404;
-      res.send('UserId not found');
-      return;
-    }
+server.delete('/users/:userId', userControllers.remove);
 
-    res.send(foundedUser);
-  } catch (err) {
-    res.send('Something went wrong:( We will fix this problem soon');
-  }
-});
-
-server.post('/users', async(req, res) => {
-  try {
-    const {
-      username,
-      first_name,
-      last_name,
-      email,
-      role,
-      state,
-    } = req.body;
-
-    if (!username
-      || !first_name
-      || !last_name
-      || !email
-      || (role !== 'user' && role !== 'admin')
-      || (state !== 'male' && state !== 'female')
-    ) {
-      res.sendStatus(422);
-
-      return;
-    }
-
-    const createdUser = await userServices.createNewUser({
-      username,
-      first_name,
-      last_name,
-      email,
-      role,
-      state,
-    });
-
-    res.statusCode = 201;
-    res.send(createdUser);
-  } catch (err) {
-    res.send('Something went wrong:( We will fix this problem soon');
-  }
-});
-
-server.delete('/users/:userId', async(req, res) => {
-  try {
-    const { userId } = req.params;
-
-    if (!Number(userId)) {
-      res.statusCode = 422;
-      res.send('UserId must be a number bigger than "0"');
-      return;
-    }
-    const foundedUser = await userServices.findUserInDB(userId);
-
-    if (!foundedUser) {
-      res.statusCode = 404;
-      res.send('UserId not found');
-      return;
-    }
-
-    const profile_id = foundedUser.profile_id;
-
-    await userServices.remove(userId, profile_id);
-
-    res.sendStatus(204);
-  } catch (err) {
-    res.send(`Something went wrong:( We will fix this problem soon`);
-  }
-});
-
-server.put('/users/:userId', async(req, res) => {
-  try {
-    const { userId } = req.params;
-
-    if (!Number(userId)) {
-      res.statusCode = 422;
-      res.send('UserId must be a number bigger than "0"');
-      return;
-    }
-
-    const {
-      username,
-      first_name,
-      last_name,
-      email,
-      role,
-      state,
-    } = req.body;
-
-    if (!username
-      || !first_name
-      || !last_name
-      || !email
-      || (role !== 'user' && role !== 'admin')
-      || (state !== 'male' && state !== 'female')
-    ) {
-      res.sendStatus(422);
-      return;
-    }
-
-    const foundedUser = await userServices.findUserInDB(userId);
-
-    if (!foundedUser) {
-      res.statusCode = 404;
-      res.send('UserId not found');
-      return;
-    }
-
-    const profile_id = foundedUser.profile_id;
-
-    const updatedUser = await userServices.update(
-      userId,
-      profile_id,
-      {
-        username,
-        first_name,
-        last_name,
-        email,
-        role,
-        state,
-      });
-
-    res.statusCode = 201;
-    res.send(updatedUser);
-
-  } catch (err) {
-    res.send(`Something went wrong:( We will fix this problem soon`);
-  }
-});
+server.put('/users/:userId', express.json(), userControllers.update);
 
 
 server.listen(port, () => {
