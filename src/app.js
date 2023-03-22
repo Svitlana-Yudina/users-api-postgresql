e0556d374623fcac57db5f80bed45419b69a5e77
Svitlana Yudina <osbka31@gmail.com>
2023-03-22 13:26:43 +0200

    services were moved to a separate file

--- src/app.js
+++ src/app.js
@@ -2,15 +2,11 @@
 /* eslint-disable no-console */
 import express from 'express';
 import path from 'path';
-import pkg from 'pg';
 import cors from 'cors';
+import * as userServices from './services/users.js';
 
 const server = express();
 const port = process.env.PORT || 5000;
-const { Client } = pkg;
-const client = new Client('postgres://qcewziyl:E4tFnzlQHFhd_aLwuFHfWGuH7fXPZGTk@trumpet.db.elephantsql.com/qcewziyl');
-
-await client.connect();
 
 server.use(cors());
 server.use(express.static(path.resolve('public')));
@@ -21,41 +17,16 @@ server.get('/users', async(req, res) => {
     const { role } = req.query;
 
     if (role === undefined) {
-      const allUsers = await client.query(`
-        SELECT
-          users.id,
-          users.username,
-          profiles.first_name,
-          profiles.last_name,
-          users.email,
-          users.role,
-          profiles.state
-        FROM users JOIN profiles
-        ON users.profile_id = profiles.id
-        ORDER BY users.id
-      `);
-
-      res.send(allUsers.rows);
+      const allUsers = await userServices.getAll();
+      
+      res.send(allUsers);
       return;
     }
 
     if (role === 'user' || role === 'admin') {
-      const usersToSend = await client.query(`
-        SELECT
-          users.id,
-          users.username,
-          profiles.first_name,
-          profiles.last_name,
-          users.email,
-          users.role,
-          profiles.state
-        FROM users JOIN profiles
-        ON users.profile_id = profiles.id
-        WHERE users.role = $1
-        ORDER BY users.id
-      `, [role]);
-
-      res.send(usersToSend.rows);
+      const usersToSend = await userServices.getByRole(role);
+      
+      res.send(usersToSend);
       return;
     }
 
@@ -76,27 +47,15 @@ server.get('/users/:userId', async(req, res) => {
       return;
     }
 
-    const foundedUser = await client.query(`
-      SELECT
-        users.id,
-        users.username,
-        profiles.first_name,
-        profiles.last_name,
-        users.email,
-        users.role,
-        profiles.state
-      FROM users JOIN profiles
-      ON users.profile_id = profiles.id
-      WHERE users.id = $1
-    `, [Number(userId)]);
+    const foundedUser = await userServices.getById(userId);
     
-    if (foundedUser.rows.length === 0) {
+    if (!foundedUser) {
       res.statusCode = 404;
       res.send('UserId not found');
       return;
     }
 
-    res.send(foundedUser.rows);
+    res.send(foundedUser);
   } catch (err) {
     res.send('Something went wrong:( We will fix this problem soon');
   }
@@ -125,42 +84,17 @@ server.post('/users', async(req, res) => {
       return;
     }
 
-    const newProfile_id = await client.query(`
-      INSERT INTO public.profiles (
-      first_name, last_name, state
-      ) VALUES ($1, $2, $3)
-       returning id`, [first_name, last_name, state]);
-
-    const profile_id = newProfile_id.rows[0].id;
-
-    const newUserId = await client.query(`
-      INSERT INTO public.users (
-        username, email, role, profile_id
-      ) VALUES ($1, $2, $3, $4)
-      returning id`, [username, email, role, profile_id]
-    );
-
-    if (!Number(newUserId.rows[0].id)) {
-      res.sendStatus(422);
-      return;
-    }
-
-    const createdUser = await client.query(`
-      SELECT
-        users.id,
-        users.username,
-        profiles.first_name,
-        profiles.last_name,
-        users.email,
-        users.role,
-        profiles.state
-      FROM users JOIN profiles
-      ON users.profile_id = profiles.id
-      WHERE users.id = (SELECT MAX(id) FROM users)
-    `);
+    const createdUser = await userServices.createNewUser({
+      username,
+      first_name,
+      last_name,
+      email,
+      role,
+      state,
+    });
 
     res.statusCode = 201;
-    res.send(createdUser.rows);
+    res.send(createdUser);
   } catch (err) {
     res.send('Something went wrong:( We will fix this problem soon');
   }
@@ -175,28 +109,17 @@ server.delete('/users/:userId', async(req, res) => {
       res.send('UserId must be a number bigger than "0"');
       return;
     }
-    const foundedUser = await client.query(`
-      SELECT * FROM users
-      WHERE users.id = $1
-    `, [Number(userId)]);
+    const foundedUser = await userServices.findUserInDB(userId);
 
-    if (foundedUser.rows.length === 0) {
+    if (!foundedUser) {
       res.statusCode = 404;
       res.send('UserId not found');
       return;
     }
 
-    const profile_id = foundedUser.rows[0].profile_id;
+    const profile_id = foundedUser.profile_id;
 
-    await client.query(`
-      DELETE FROM users
-      WHERE id = $1;
-    `, [Number(userId)]);
-
-    await client.query(`
-      DELETE FROM profiles
-      WHERE id = $1;
-    `, [profile_id]);
+    await userServices.remove(userId, profile_id);
 
     res.sendStatus(204);
   } catch (err) {
@@ -234,51 +157,30 @@ server.put('/users/:userId', async(req, res) => {
       return;
     }
 
-    const foundedUser = await client.query(`
-      SELECT * FROM users
-      WHERE users.id = $1
-    `, [Number(userId)]);
+    const foundedUser = await userServices.findUserInDB(userId);
 
-    if (foundedUser.rows.length === 0) {
+    if (!foundedUser) {
       res.statusCode = 404;
       res.send('UserId not found');
       return;
     }
 
-    const profile_id = foundedUser.rows[0].profile_id;
+    const profile_id = foundedUser.profile_id;
 
-    await client.query(`
-      UPDATE users
-      SET username = $1,
-          email = $2,
-          role = $3
-      WHERE id = $4;
-    `, [username, email, role, Number(userId)]);
-
-    await client.query(`
-      UPDATE profiles
-      SET first_name = $1,
-          last_name = $2,
-          state = $3
-      WHERE id = $4;
-    `, [first_name, last_name, state, profile_id]);
-
-    const updatedUser = await client.query(`
-      SELECT
-        users.id,
-        users.username,
-        profiles.first_name,
-        profiles.last_name,
-        users.email,
-        users.role,
-        profiles.state
-      FROM users JOIN profiles
-      ON users.profile_id = profiles.id
-      WHERE users.id = $1
-    `, [Number(userId)]);
+    const updatedUser = await userServices.update(
+      userId,
+      profile_id,
+      {
+        username,
+        first_name,
+        last_name,
+        email,
+        role,
+        state,
+      });
 
     res.statusCode = 201;
-    res.send(updatedUser.rows);
+    res.send(updatedUser);
 
   } catch (err) {
     res.send(`Something went wrong:( We will fix this problem soon`);
